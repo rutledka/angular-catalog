414e869482f22c9f3c2ca0fb739b5974d24cea92
rutledka <khalilrutledge@gmail.com>
2015-11-29 10:45:50 -0500

    add to cart functionality

--- js/grunts/controllers.js
+++ js/grunts/controllers.js
@@ -2,19 +2,29 @@
 
 angular.module('storeApp')
 .controller ('mainCtrl', function($scope, dataService) {
-  //get the products
+  //initialize our cart and overlay variables
   $scope.overlay = {
     "isActive": false,
   }
+
+  $scope.cart = [];
+
+  //get the products
   dataService.getProducts(function(response) {
     console.log(response.data);
     $scope.products = response.data;
   });
 
-  $scope.addToCart = function(product) {
+  //view product details - accepts product object as argument - replaces overlay.product object with new product
+  $scope.viewDetails = function(product) {
     console.log(product);
     $scope.overlay.isActive = !$scope.overlay.isActive;
     $scope.overlay.product = product;
     console.log($scope.overlay);
-  }
+  };
+
+  //add to cart - accepts product object as argument - pushes that object into our cart array
+  $scope.addToCart = function(product) {
+    $scope.cart.push(product);
+  };
 });
